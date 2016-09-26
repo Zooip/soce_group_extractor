eb261f61720571b4fe81c8cd3d991f6104c449b8
Alexandre Narbonne <alexandre.narbonne@gadz.org>
2016-09-26 15:42:34 +0200

    Add environment config and run bin file

--- soce_group_extractor.rb
+++ soce_group_extractor.rb
@@ -3,11 +3,22 @@ $:.unshift File.dirname(__FILE__)
 require 'extra_config'
 require 'gorg_message_sender'
 
-DB_CONFIG=ExtraConfig.new(File.expand_path("database.yml",File.dirname(__FILE__)),"SGE_DB")
-RMQ_CONFIG=ExtraConfig.new(File.expand_path("rabbitmq.yml",File.dirname(__FILE__)),"SGE_RMQ")
+CURRENT_ENV=ENV['SGE_ENV']||'no_env'
+
+DB_CONFIG=ExtraConfig.new(File.expand_path("database.yml",File.dirname(__FILE__)),"SGE_DB",CURRENT_ENV)
+RMQ_CONFIG=ExtraConfig.new(File.expand_path("rabbitmq.yml",File.dirname(__FILE__)),"SGE_RMQ",CURRENT_ENV)
 require 'soce_group_extractor/rabbit_mq_config'
 
 class SoceGroupExtractor
+
+  def process
+    sender=GorgMessageSender.new
+    SoceGroup.retrieve_all.each do |sg|
+      self.class.logger.info "Processing #{sg.nom_groupe}"
+      GramGroup.from_soce_group(sg).commit(message_sender: sender)
+      self.class.logger.info "Process #{sg.nom_groupe} done"
+    end
+  end
 
   def self.mysql_conn
     @conn||= Mysql2::Client.new(
@@ -19,11 +30,9 @@ class SoceGroupExtractor
     )
   end
 
-  def process
-    sender=GorgMessageSender.new
-    SoceGroup.retrieve_all.each do |sg|
-      GramGroup.from_soce_group(sg).commit(message_sender: sender)
-    end
+
+  def self.logger
+    @logger||=Logger.new(STDOUT)
   end
 end
 
